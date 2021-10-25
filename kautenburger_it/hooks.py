73223732f97f52f2d9dfde6aa7377965e898f85d
Andreas Schneider <andreas@yourdisposal.de>
2021-10-25 14:11:55 +0200

    Changed all hooks.py:includes to list as frappe desk.html expects lists in *_include_*. It should have added a css-include for every letter, though..

--- kautenburger_it/hooks.py
+++ kautenburger_it/hooks.py
@@ -15,12 +15,12 @@ app_license = "GPLv3"
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = "/assets/kautenburger_it/css/task.css"
-# app_include_js = "/assets/kautenburger_it/js/kautenburger_it.js"
+app_include_css = ["/assets/kautenburger_it/css/task.css"]
+# app_include_js = ["/assets/kautenburger_it/js/kautenburger_it.js"]
 
 # include js, css files in header of web template
-# web_include_css = "/assets/kautenburger_it/css/kautenburger_it.css"
-# web_include_js = "/assets/kautenburger_it/js/kautenburger_it.js"
+# web_include_css = ["/assets/kautenburger_it/css/kautenburger_it.css"]
+# web_include_js = ["/assets/kautenburger_it/js/kautenburger_it.js"]
 
 # include js in page
 # page_js = {"page" : "public/js/file.js"}
